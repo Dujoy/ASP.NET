d0a5b6476cb0562dc659d4de4061d67e7f4df125
Dujoy <durjoysarker520@gmail.com>
2022-05-25 01:02:31 +0600

    Assignment1

---
+++ Mycv/Mycv/Controllers/MycvController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mycv.Controllers
+{
+    public class MycvController : Controller
+    {
+        // GET: Mycv
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NE()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+    }
+}
